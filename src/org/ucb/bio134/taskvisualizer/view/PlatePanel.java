6dd5b79ce01ab5b64f3d9e1efc73575887302824
Lucas Waldburger <lucas.waldburger@ucsf.edu>
2017-11-21 20:16:35 -0800

    Update PlatePanel.java

--- src/org/ucb/bio134/taskvisualizer/view/PlatePanel.java
+++ src/org/ucb/bio134/taskvisualizer/view/PlatePanel.java
@@ -1,105 +1,158 @@
-//package org.ucb.taskvisualizer.view;
-//
-//import BlockConfig;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class PlatePanel extends JPanel {
-//    private JPanel currentDisplay;
-//    private BlockConfig plateConfig;
-//
-//    public PlatePanel() {
-//        setLayout(null);
-//        currentDisplay = createEmptyWell();
-//        removeWell();
-//    }
-//    private JPanel createEmptyWell() {
+package org.ucb.bio134.taskvisualizer.view.panels;
+
+import org.ucb.bio134.taskvisualizer.model.*;
+import org.ucb.bio134.taskvisualizer.view.View;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author J. Christopher Anderson
+ */
+public class PlatePanel extends JPanel {
+    private JPanel currentDisplay;
+    private Config config;
+    private WellPanel[][] wells;
+    private Color green;
+
+
+    public PlatePanel(ContainerType type) {
+        setLayout(null);
+        calcConfig(type);
+        currentDisplay = createEmptyPosition();
+        green = new Color(76,153,0);
+        wells = new WellPanel[config.getNumRows()][config.getNumCols()];
+        removePlate();
+    }
+
+    private void calcConfig(ContainerType type) {
+        if (type == ContainerType.PCR) {
+            this.config = PCRPlateConfig.getInstance();
+        } else if (type == ContainerType.TUBE) {
+            this.config = TubePlateConfig.getInstance();
+        } else {
+            throw new IllegalArgumentException("Invalid configuration type");
+        }
+    }
+
+    private JPanel createEmptyPosition() {
+        JPanel out = new JPanel();
+        out.setBackground(Color.PINK);
+        JLabel label = new JLabel("Empty");
+        out.add(label, BorderLayout.CENTER);
+        out.setBounds(0, 0, config.getWidth(), config.getHeight());
+        return out;
+    }
+
+    private JPanel createAddPlatePanel(String platename) {
+        JPanel out = new JPanel();
+        out.setBackground(new Color(0,0,50));
+        JLabel label = new JLabel(platename);
+        label.setForeground(Color.WHITE);
+        out.add(label, BorderLayout.CENTER);
+        out.setBounds(0, 0, config.getWidth()/config.getNumCols(), config.getHeight()/config.getNumRows());
+        return out;
+    }
+
+    public WellPanel[][] addWells(int rows, int cols) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int xpos = View.windowOffset + col * config.getSubBlockWidth();
+                int ypos = View.windowOffset + row * config.getSubBlockHeight();
+                //Create and add the panel
+                wells[row][col] = new WellPanel(green);
+                wells[row][col].setBounds(xpos ,ypos , config.getSubBlockWidth(), config.getSubBlockHeight());
+            }
+        }
+        System.out.println(wells);
+        return wells;
+    }
+
+    private JPanel createBlackPanel() {
+        JPanel out = new JPanel();
+        out.setLayout(null);
+        out.setBackground(Color.BLACK);
+        out.setBounds(0, 0, config.getWidth(), config.getHeight());
+        return out;
+    }
+
+//    private JPanel createWellPanel(Color color, int row, int col) {
 //        JPanel out = new JPanel();
+//        out.setBackground(color);
 //
-////        out.setBackground(Color.WHITE);
-////        JLabel label = new JLabel("WELL");
-////        out.add(label, BorderLayout.CENTER);
-////        out.setBounds(0, 0, View.wellWidth, View.wellHeight);
+//        double posX = config.getXoffset() + (col-0.5) * config.getWellWidth();
+//        double posy = config.getYoffset() + (row-0.5) * config.getWellHeight();
+//
+//        int width = DeckConfig.calcPixels(plateConfig.getWellWidth());
+//        int height = DeckConfig.calcPixels(plateConfig.getWellHeight());
+//
+//        out.setBounds(DeckConfig.calcPixels(posX), DeckConfig.calcPixels(posy), width, height);
 //        return out;
 //    }
-//    private JPanel createAddPlatePanel(String platename) {
-//        JPanel out = new JPanel();
-//        out.setBackground(new Color(0,0,50));
-//        JLabel label = new JLabel(platename);
-//        label.setForeground(Color.WHITE);
-//        out.add(label, BorderLayout.CENTER);
-//        out.setBounds(0, 0, View.wellWidth, View.wellHeight);
-//        return out;
-//    }
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        g.setColor(Color.PINK);
-//        g.fillOval(0, 0, getWidth(), getHeight());
-//    }
-//    public void reset() {
+
+    //VIEW-RELAYED COMMANDS//
+
+    public void reset() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                removeAll();
+                setBorder(null);
+                add(currentDisplay);
+                revalidate();
+                repaint();
+            }
+        });
+    }
+
+    public void addPlate(String plateName) {
+        currentDisplay = this.createAddPlatePanel(plateName);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                removeAll();
+                add(currentDisplay);
+                revalidate();
+                repaint();
+            }
+        });
+    }
+
+//    public void colorWell(Color color, int wellRow, int wellCol) {
+//        JPanel blackPanel = createBlackPanel();
+//        blackPanel.setBorder(BorderFactory.createLineBorder(color));
+//        JPanel wellPanel = createWellPanel(color, wellRow, wellCol);
 //        SwingUtilities.invokeLater(new Runnable() {
 //            @Override
 //            public void run() {
 //                removeAll();
-//                setBorder(null);
-//                add(currentDisplay);
+//                add(blackPanel);
+//
+//                blackPanel.add(wellPanel);
 //                revalidate();
 //                repaint();
 //            }
 //        });
 //    }
-//
-//    public void addWell(String plateName) {
-//        currentDisplay = this.createAddPlatePanel(plateName);
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                removeAll();
-//                add(currentDisplay);
-//                revalidate();
-//                repaint();
-//            }
-//        });
-//    }
-////    public void displayWell(Color color, int rows, int cols) {
-////        JPanel blackPanel = createBlackPanel();
-////        blackPanel.setBorder(BorderFactory.createLineBorder(color));
-////        for (int i = 0; i < rows; i++) {
-////            for (int j = 0; j < cols; j++) {
-////                JPanel wellPanel = createWellPanel(color, i, j);
-////                SwingUtilities.invokeLater(new Runnable() {
-////                    @Override
-////                    public void run() {
-////                        removeAll();
-////                        add(blackPanel);
-////
-////                        blackPanel.add(wellPanel);
-////                        revalidate();
-////                        repaint();
-////                    }
-////                });
-////            }
-////        }
-//
-//    //    }
-//
-//    //Relay requested from View in response to an "removePlate" step
-//    private void removeWell() {
-//        currentDisplay = this.createEmptyWell();
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                removeAll();
-//                add(currentDisplay);
-//                revalidate();
-//                repaint();
-//            }
-//        });
-//    }
-//
-//
-//
-//}
-//
-//}
+
+    //Relay requested from View in response to an "removePlate" step
+    private void removePlate() {
+        currentDisplay = this.createEmptyPosition();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                removeAll();
+                add(currentDisplay);
+                revalidate();
+                repaint();
+            }
+        });
+    }
+
+
+}
